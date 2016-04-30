9df649cca1c444ab93f2e4a4472226a73d4d49ad
pointcache <antonov.3d@gmail.com>
2016-04-30 17:39:22 -0500

    init commit

---
+++ Assets/UnityLib/Constraints/PositionConstraint.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityLib.Constraints
+{
+    /// <summary>
+    /// Constraints axis of host object to position of target object.
+    /// </summary>
+    public class PositionConstraint : MonoBehaviour
+    {
+        public Vector3 targetPosition; // The current position to constraint to, can be set manually if the target Transform is null
+        public Transform targetTransform; // Current target transform to constrain to, can be left null for use of provided Vector3
+
+        public bool X, Y, Z;
+
+        private Transform _tr;
+        public Transform tr { get { if (!_tr) _tr = GetComponent<Transform>(); return _tr; } }
+
+        void Update()
+        {
+            if(targetTransform != null)
+            {
+                targetPosition = targetTransform.position;
+            }
+            Vector3 pos = tr.position;
+            if (X)
+            {
+                pos.x = targetPosition.x;
+            }
+            if (Y)
+            {
+                pos.y = targetPosition.y;
+            }
+            if (Z)
+            {
+                pos.z = targetPosition.z;
+            }
+            tr.position = pos;
+        }   
+    }
+}
---
+++ Assets/UnityLib/Constraints/RotationConstraint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace UnityLib.Constraints
+{
+    public class RotationConstraint : MonoBehaviour
+    {
+        /* to keep the rotation on one axis at zero, remove the target transform and enable that axis*/
+
+        public Vector3 targetRotation; // The current rotation to constraint to, can be set manually if the target Transform is null
+        public Transform targetTransform; // Current target transform to constrain to, can be left null for use of provided Vector3
+
+        public bool X, Y, Z;
+
+        private Transform _tr;
+        public Transform tr { get { if (!_tr) _tr = GetComponent<Transform>(); return _tr; } }
+
+        void Update()
+        {
+            if (targetTransform != null)
+            {
+                targetRotation = targetTransform.rotation.eulerAngles;
+            }
+            Vector3 rot = tr.rotation.eulerAngles;
+            if (X)
+            {
+                rot.x = targetRotation.x;
+            }
+            if (Y)
+            {
+                rot.y = targetRotation.y;
+            }
+            if (Z)
+            {
+                rot.z = targetRotation.z;
+            }
+            tr.rotation = Quaternion.Euler(rot);
+        }
+    }
+}
